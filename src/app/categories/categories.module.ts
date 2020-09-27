557517b0a0618bb2021ab8d20825d7a15298d7f0
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2020-09-27 18:42:14 -0300

    Adicionado material design input color picker

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialDesignSharedModule } from '../material-design-shared/material-design-shared.module';
+import { NgxMatColorPickerModule, MAT_COLOR_FORMATS, NGX_MAT_COLOR_FORMATS } from '@angular-material-components/color-picker';
 
 import { CategoryCreateComponent } from './category-create/category-create.component';
 import { CategoriesListComponent } from './categories-list/categories-list.component';
@@ -19,7 +20,11 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MaterialDesignSharedModule
+    MaterialDesignSharedModule,
+    NgxMatColorPickerModule
+  ],
+  providers: [
+    { provide: MAT_COLOR_FORMATS, useValue: NGX_MAT_COLOR_FORMATS }
   ]
 })
 export class CategoriesModule { }
